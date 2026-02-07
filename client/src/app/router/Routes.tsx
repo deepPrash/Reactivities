5094c20a109445a1b99f8a8e5116a3d88143129b
Deepthi Suk <deepstrvm@gmail.com>
2026-02-07 17:42:20 -0500

    End of Section 8

--- client/src/app/router/Routes.tsx
+++ client/src/app/router/Routes.tsx
@@ -2,18 +2,19 @@ import { createBrowserRouter } from "react-router";
 import App from "../layout/App";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
 import ActivityForm from "../../features/activities/form/ActivityForm";
-import ActivityDetails from "../../features/activities/details/ActivityDetails";
+import ActivityDetailPage from "../../features/activities/details/ActivityDetailPage";
+import HomePage from "../../features/home/HomePage";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
     children: [
-      { path: "", element: <h1>Home Page</h1> },
       { path: "activities", element: <ActivityDashboard /> },
-      { path: "activities/:id", element: <ActivityDetails /> },
+      { path: "activities/:id", element: <ActivityDetailPage /> },
       { path: "createActivity", element: <ActivityForm key="create" /> },
       { path: "manage/:id", element: <ActivityForm key="manage" /> },
+      { path: "", element: <HomePage /> },
     ],
   },
 ]);
