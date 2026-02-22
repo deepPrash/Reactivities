61a6569e9283886f3ef70a8ee4b160a3e6d34835
Deepthi Suk <deepstrvm@gmail.com>
2026-02-22 13:55:25 -0500

    End of section 15

--- client/src/features/activities/dashboard/ActivityCard.tsx
+++ client/src/features/activities/dashboard/ActivityCard.tsx
@@ -14,19 +14,20 @@ import {
 
 import { Link } from "react-router";
 import { formatDate } from "../../../lib/util/util";
+import AvatarPopover from "../../../app/shared/components/AvatarPopover";
 
 type Props = {
   activity: Activity;
 };
 
 export default function ActivityCard({ activity }: Props) {
-  const isHost = false;
-  const isGoing = false;
-  const label = isHost
-    ? "You are hosting this activity"
-    : "You are going to this activity";
-  const isCancelled = false;
-  const color = isHost ? "secondary" : isGoing ? "warning" : "default";
+  const label = activity.isHost ? "You are hosting" : "You are going";
+
+  const color = activity.isHost
+    ? "secondary"
+    : activity.isGoing
+      ? "warning"
+      : "default";
 
   return (
     <Card elevation={3} sx={{ borderRadius: 3 }}>
@@ -41,16 +42,23 @@ export default function ActivityCard({ activity }: Props) {
           }}
           subheader={
             <>
-              Hosted by {"  "} <Link to={`/profiles/bob`}>Bob</Link>
+              Hosted by {"  "}{" "}
+              <Link to={`/profiles/${activity.hostId}`}>
+                {activity.hostDisplayName}
+              </Link>
             </>
           }
         />
         <Box display="flex" flexDirection="column" gap={2} mr={2}>
-          {isHost ||
-            (isGoing && (
-              <Chip label={label} color={color} sx={{ borderRadius: 2 }} />
-            ))}
-          {isCancelled && (
+          {(activity.isHost || activity.isGoing) && (
+            <Chip
+              variant="outlined"
+              label={label}
+              color={color}
+              sx={{ borderRadius: 2 }}
+            />
+          )}
+          {activity.isCancelled && (
             <Chip label="Cancelled" color="error" sx={{ borderRadius: 2 }} />
           )}
         </Box>
@@ -60,7 +68,7 @@ export default function ActivityCard({ activity }: Props) {
 
       <CardContent sx={{ p: 0 }}>
         <Box display="flex" alignItems="center" mb={2} px={2}>
-          <Box display="flex" flexgrow={0} alignItems="center">
+          <Box display="flex" flexGrow={0} alignItems="center">
             <AccessTime sx={{ mr: 1 }} />
             <Typography variant="body2" noWrap>
               {formatDate(activity.date)}
@@ -76,7 +84,9 @@ export default function ActivityCard({ activity }: Props) {
           gap={2}
           sx={{ backgroundColor: "grey.200", py: 3, pl: 3 }}
         >
-          Attendees go here
+          {activity.attendees.map((attendee) => (
+            <AvatarPopover key={attendee.id} profile={attendee} />
+          ))}
         </Box>
       </CardContent>
       <CardContent sx={{ pb: 2 }}>
--- client/src/lib/hooks/useActivities.ts
+++ client/src/lib/hooks/useActivities.ts
@@ -17,6 +17,15 @@ export const useActivities = (id?: string) => {
     //staleTime: 1000 * 60 * 5, // 5 minutes
     enabled: !id && location.pathname === "/activities" && !!currentUser,
     // Only fetch activities list if we're on the activities page and user is logged in
+    select: (data) => {
+      return data.map((activity) => {
+        return {
+          ...activity,
+          isHost: currentUser?.id === activity.hostId,
+          isGoing: activity.attendees.some((a) => a.id === currentUser?.id),
+        };
+      });
+    },
   });
 
   const { data: activity, isLoading: isLoadingActivity } = useQuery({
@@ -27,6 +36,13 @@ export const useActivities = (id?: string) => {
     },
     enabled: !!id && !!currentUser,
     // Only fetch activity details if an ID is provided and user is logged in
+    select: (data) => {
+      return {
+        ...data,
+        isHost: currentUser?.id === data.hostId,
+        isGoing: data.attendees.some((a) => a.id === currentUser?.id),
+      };
+    },
   });
 
   const updateActivity = useMutation({
@@ -57,6 +73,63 @@ export const useActivities = (id?: string) => {
     },
   });
 
+  const updateAttendance = useMutation({
+    mutationFn: async (id: string) => {
+      await agent.post(`/activities/${id}/attend`);
+    },
+    onMutate: async (activityId: string) => {
+      await queryClient.cancelQueries({ queryKey: ["activities", activityId] });
+      const prevActivity = queryClient.getQueryData<Activity>([
+        "activities",
+        activityId,
+      ]);
+
+      queryClient.setQueryData<Activity>(
+        ["activities", activityId],
+        (oldActivity) => {
+          if (!oldActivity || !currentUser) {
+            return oldActivity;
+          }
+          const isHost = oldActivity.hostId === currentUser.id;
+
+          const isAttending = oldActivity.attendees.some(
+            (a) => a.id === currentUser?.id,
+          );
+
+          return {
+            ...oldActivity,
+            isCancelled: isHost
+              ? !oldActivity.isCancelled
+              : oldActivity.isCancelled,
+            attendees: isAttending
+              ? isHost
+                ? oldActivity.attendees
+                : oldActivity.attendees.filter((x) => x.id !== currentUser.id)
+              : [
+                  ...oldActivity.attendees,
+                  {
+                    id: currentUser.id,
+                    displayName: currentUser.displayName,
+                    imageUrl: currentUser.imageUrl,
+                  },
+                ],
+          };
+        },
+      );
+      return { prevActivity };
+    },
+    onError: async (err, activityId, context) => {
+      console.log("Prev Activity : " + context?.prevActivity);
+      console.log(err);
+      if (context?.prevActivity) {
+        queryClient.setQueryData(
+          ["activities", activityId],
+          context.prevActivity,
+        );
+      }
+    },
+  });
+
   return {
     activities,
     isLoading,
@@ -65,5 +138,6 @@ export const useActivities = (id?: string) => {
     deleteActivity,
     isLoadingActivity,
     activity,
+    updateAttendance,
   };
 };
